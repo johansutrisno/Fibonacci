05ecde8f9932d71e99dada133168dea9ad7092e9
JohanSutrisno_6706161099 <johansutrisno30@gmail.com>
2018-03-26 09:11:03 +0700

    Fibonacci v.1.1

---
+++ Jurnal/src/jurnal/Jurnal.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Jurnal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        int first = 0;
+        int last = 1;
+        int x;
+        
+        System.out.println("Input :");
+        x = in.nextInt();
+        
+        System.out.println("Output:");
+        if (x > 0) {
+            for (int i = 0; i < x; i++) {
+                System.out.print(last + " ");
+                int temp = last;
+                last += first;
+                first = temp;
+            }
+        }
+    }
+    
+}
